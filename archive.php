cc8233b4dc82248215b298d2d432b5b43e81a9a0
Douglas Rosa <douglasanro@gmail.com>
2017-11-05 19:18:55 -0200

    add new WordPress files

---
+++ archive.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package simple-wp-gulp
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+get_header(); ?>
+<?php get_footer();
---
+++ comments.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package simple-wp-gulp
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+?>
